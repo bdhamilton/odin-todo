63ea4f87003ee1608de064336a1e1e414285494c
Brian Hamilton <bdhamilton@gmail.com>
2024-05-21 13:39:31 -0400

    Write project class

--- src/manageTodos.js
+++ src/manageTodos.js
@@ -3,7 +3,7 @@ class Todo {
     if (title === undefined) {
       console.log("You need at least a title!");
       return;
-    };
+    }
 
     this.title = title;
     this.projectID = projectID;
@@ -28,6 +28,48 @@ class Todo {
 
   toggleComplete() {
     this.completed = this.completed === false ? true : false;
+  }
+
+  display() {
+    console.table(this);
+  }
+}
+
+/* 
+CONSIDER:
+The fact that these two classes are identical makes me think there's
+a simpler way of storing this data. Should projects be an extension
+of the todo class? That feels strange because projects _contain_ todos, 
+but I'm not sure that subclasses necessary need to be child classes.
+The only difference between the two classes is that projects include a
+list of todos inside them.
+
+For now I might actually just simplify projects. 
+*/
+
+class Project {
+  constructor(title, description) {
+    if (title === undefined) {
+      console.log("You need at least a title!");
+      return;
+    }
+
+    this.title = title;
+    this.description = description;
+    this.todos = [];  // This blank array is where we'll keep our list of todos.
+  }
+
+  update(field, value) {
+    // Make sure we're updating a legitimate field.
+    if (field === "title" || field === "description") {
+      if (field === "title" && !value) {
+        console.log("You can't set a todo's title to blank.");
+      } else {
+        this[field] = value;
+      }
+    } else {
+      console.log("You can only update titles and descriptions.");
+    }
   }
 
   display() {
